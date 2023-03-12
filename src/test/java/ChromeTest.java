3b133c958219f00e59eca8142483f82c7c8ed590
Hiyot <hiyot95@gmail.com>
2023-03-12 11:47:16 +0200

    Rename Calss

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class ChromeExcess {
+public class ChromeTest {
 
     private static WebDriver driver;
 
